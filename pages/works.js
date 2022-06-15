2d3c615bf9328a7811efd7c6809c5feb8b77c7d1
thesyntaxninja <pdesmay@gmail.com>
2022-06-15 15:49:32 -0700

    delay web3 section works.js

--- pages/works.js
+++ pages/works.js
@@ -47,43 +47,45 @@ const Works = () => {
                     />
                 </Animate>
             </SimpleGrid>
-            <Heading
-                fontWeight={'regular'}
-                pb={'1rem'}
-                borderBottom={'1px solid'}
-                borderBottomColor={useColorModeValue('darkText', 'lightText')}
-                mb={'2rem'}
-                w={'max-content'}
-            >
-                Web3
-            </Heading>
-            <SimpleGrid columns={{base: 1, md: 2}} spacingX={10} spacingY={6} mb={'3rem'}>
-                <Animate delay={.4}>
-                    <Card
-                        link={'https://www.drifternovel.com/'}
-                        imgSrc={'/images/works/drifters.png'}
-                        headingText={'Drifters of Alta'}
-                        descriptionText={'A PFP NFT based on the characters from the first full length fantasy novel ' +
-                            'on the Ethereum blockchain.'}
-                    />
-                </Animate>
-                <Animate delay={.5}>
-                    <Card
-                        link={'https://astro-dao.vercel.app/'}
-                        imgSrc={'/images/works/astrodao.png'}
-                        headingText={'Astro DAO'}
-                        descriptionText={'An NFT gated membership DAO complete with an ERC-20 token drop.'}
-                    />
-                </Animate>
-                <Animate delay={.6}>
-                    <Card
-                        link={'https://booger-boys.vercel.app/'}
-                        imgSrc={'/images/works/boogerboys.png'}
-                        headingText={'Booger Boys'}
-                        descriptionText={'A PFP NFT drop on Solana.'}
-                    />
-                </Animate>
-            </SimpleGrid>
+            <Animate delay={.3}>
+                <Heading
+                    fontWeight={'regular'}
+                    pb={'1rem'}
+                    borderBottom={'1px solid'}
+                    borderBottomColor={useColorModeValue('darkText', 'lightText')}
+                    mb={'2rem'}
+                    w={'max-content'}
+                >
+                    Web3
+                </Heading>
+                <SimpleGrid columns={{base: 1, md: 2}} spacingX={10} spacingY={6} mb={'3rem'}>
+                    <Animate delay={.4}>
+                        <Card
+                            link={'https://www.drifternovel.com/'}
+                            imgSrc={'/images/works/drifters.png'}
+                            headingText={'Drifters of Alta'}
+                            descriptionText={'A PFP NFT based on the characters from the first full length fantasy novel ' +
+                                'on the Ethereum blockchain.'}
+                        />
+                    </Animate>
+                    <Animate delay={.5}>
+                        <Card
+                            link={'https://astro-dao.vercel.app/'}
+                            imgSrc={'/images/works/astrodao.png'}
+                            headingText={'Astro DAO'}
+                            descriptionText={'An NFT gated membership DAO complete with an ERC-20 token drop.'}
+                        />
+                    </Animate>
+                    <Animate delay={.6}>
+                        <Card
+                            link={'https://booger-boys.vercel.app/'}
+                            imgSrc={'/images/works/boogerboys.png'}
+                            headingText={'Booger Boys'}
+                            descriptionText={'A PFP NFT drop on Solana.'}
+                        />
+                    </Animate>
+                </SimpleGrid>
+            </Animate>
         </Layout>
     )
 }
