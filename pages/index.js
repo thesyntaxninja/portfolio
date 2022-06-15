c90ffa81e9a286d550f789c984010dbfde30cd8e
thesyntaxninja <pdesmay@gmail.com>
2022-06-15 15:03:55 -0700

    text edits and Animate.js component

--- pages/index.js
+++ pages/index.js
@@ -12,151 +12,154 @@ import {
     ListItem,
     useColorModeValue,
 } from "@chakra-ui/react"
-import Layout from '../components/layouts/Main'
+import Layout from "../components/layouts/Main"
 import {ChevronRightIcon} from "@chakra-ui/icons"
 import {FaTwitter, FaGithub} from "react-icons/fa"
 import NextLink from "next/link"
+import Animate from "/components/Animate"
 
 const Home = () => {
     return (
         <Layout>
-            <Heading
-                fontWeight={'regular'}
-                pb={'1rem'}
-                borderBottom={'1px solid'}
-                borderBottomColor={useColorModeValue('darkText', 'lightText')}
-                mb={'2rem'}
-                w={'max-content'}
-            >
-                Work
-            </Heading>
-            <Text mb={'1rem'}>
-                Parker is an entrepreneur and a full-stack developer based in Arizona with a passion for building digital
-                services/web3 projects with a beautiful user experience. He has a knack for all things launching products,
-                from planning and designing all the way to solving real-life problems with code. When not online,
-                he loves hanging out with his family and all things space related. Currently,
-                he is working as the founder of a software development agency called Blue Hare.
-            </Text>
-            <Flex justifyContent={'center'}>
-                <NextLink href="/works" scroll={false}>
-                    <Button
-                        colorScheme={'teal'}
-                        rightIcon={<ChevronRightIcon />}
-                        mb={'3rem'}
-                        textTransform={'uppercase'}
-                        href="/works"
-                    >
-                        My Portfolio
-                    </Button>
-                </NextLink>
-            </Flex>
-            <Heading
-                fontWeight={'regular'}
-                pb={'1rem'}
-                borderBottom={'1px solid'}
-                borderBottomColor={useColorModeValue('darkText', 'lightText')}
-                mb={'2rem'}
-                w={'max-content'}
-            >
-                Bio
-            </Heading>
-            <Grid templateColumns='repeat(6, 1fr)' gap={'16px'} mb={'3rem'}>
-                <GridItem colSpan={2}>
-                    <Text fontWeight={'bold'}>
-                        1988
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={4}>
-                    <Text>
-                        Born in Arizona
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={2}>
-                    <Text fontWeight={'bold'}>
-                        2015
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={4}>
-                    <Text>
-                        Completed the Master’s program in Software Engineering at Brandeis University
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={2}>
-                    <Text fontWeight={'bold'}>
-                        2015
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={4}>
-                    <Text>
-                        Technical project lead at Core Online Training
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={2}>
-                    <Text fontWeight={'bold'}>
-                        2016
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={4}>
-                    <Text>
-                        Director of Technology at Bedrock Financial Services
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={2}>
-                    <Text fontWeight={'bold'}>
-                        2016 to present
-                    </Text>
-                </GridItem>
-                <GridItem colSpan={4}>
-                    <Text>
-                        Founder of Blue Hare
-                    </Text>
-                </GridItem>
-            </Grid>
-            <Heading
-                fontWeight={'regular'}
-                pb={'1rem'}
-                borderBottom={'1px solid'}
-                borderBottomColor={useColorModeValue('darkText', 'lightText')}
-                mb={'2rem'}
-                w={'max-content'}
-            >
-                On the web
-            </Heading>
-            <Flex
-                mb={'80px'}
-                justifyContent={'space-between'}
-                flexDirection={{base: 'column-reverse', md: 'row'}}
-            >
-                <Image
-                    pt={{base: '16px', md: 0}}
-                    src={useColorModeValue('/images/home/info-light.svg', '/images/home/info-dark.svg')}
-                    alignSelf={{base: 'center'}}
-                />
-                <List spacing={'16px'}>
-                    <ListItem>
-                        <Link href="https://twitter.com/parkeresmay" target="_blank">
-                            <Button
-                                variant="ghost"
-                                colorScheme="teal"
-                                leftIcon={<FaTwitter />}
-                            >
-                                @parkeresmay
-                            </Button>
-                        </Link>
-                    </ListItem>
-                    <ListItem>
-                        <Link href="https://github.com/thesyntaxninja" target="_blank">
-                            <Button
-                                variant="ghost"
-                                colorScheme="teal"
-                                leftIcon={<FaGithub />}
-                            >
-                                @thesyntaxninja
-                            </Button>
-                        </Link>
-                    </ListItem>
-                </List>
-            </Flex>
+            <Animate delay={.1}>
+                <Heading
+                    fontWeight={'regular'}
+                    pb={'1rem'}
+                    borderBottom={'1px solid'}
+                    borderBottomColor={useColorModeValue('darkText', 'lightText')}
+                    mb={'2rem'}
+                    w={'max-content'}
+                >
+                    Work
+                </Heading>
+                <Text mb={'1rem'}>
+                    Parker is an entrepreneur and software engineer with a passion for building digital/web3
+                    products with beautiful user experiences. He can work his way up and down the stack, but has a passion
+                    for frontend development. He has a knack for all things launching products, from planning and designing
+                    to solving real-life problems with code. When not online, he loves hanging out with his family and all
+                    things space related. Currently, he is working as the founder of a software development agency called Blue Hare.
+                </Text>
+                <Flex justifyContent={'center'}>
+                    <NextLink href="/works" scroll={false}>
+                        <Button
+                            colorScheme={'teal'}
+                            rightIcon={<ChevronRightIcon />}
+                            mb={'3rem'}
+                            textTransform={'uppercase'}
+                            href="/works"
+                        >
+                            My Portfolio
+                        </Button>
+                    </NextLink>
+                </Flex>
+                <Heading
+                    fontWeight={'regular'}
+                    pb={'1rem'}
+                    borderBottom={'1px solid'}
+                    borderBottomColor={useColorModeValue('darkText', 'lightText')}
+                    mb={'2rem'}
+                    w={'max-content'}
+                >
+                    Bio
+                </Heading>
+                <Grid templateColumns='repeat(6, 1fr)' gap={'16px'} mb={'3rem'}>
+                    <GridItem colSpan={2}>
+                        <Text fontWeight={'bold'}>
+                            1988
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={4}>
+                        <Text>
+                            Born in Arizona
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={2}>
+                        <Text fontWeight={'bold'}>
+                            2015
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={4}>
+                        <Text>
+                            Completed the Master’s program in Software Engineering at Brandeis University
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={2}>
+                        <Text fontWeight={'bold'}>
+                            2015
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={4}>
+                        <Text>
+                            Technical project lead at Core Online Training
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={2}>
+                        <Text fontWeight={'bold'}>
+                            2016
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={4}>
+                        <Text>
+                            Director of Technology at Bedrock Financial Services
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={2}>
+                        <Text fontWeight={'bold'}>
+                            2016 to present
+                        </Text>
+                    </GridItem>
+                    <GridItem colSpan={4}>
+                        <Text>
+                            Founder of Blue Hare
+                        </Text>
+                    </GridItem>
+                </Grid>
+                <Heading
+                    fontWeight={'regular'}
+                    pb={'1rem'}
+                    borderBottom={'1px solid'}
+                    borderBottomColor={useColorModeValue('darkText', 'lightText')}
+                    mb={'2rem'}
+                    w={'max-content'}
+                >
+                    On the web
+                </Heading>
+                <Flex
+                    mb={'80px'}
+                    justifyContent={'space-between'}
+                    flexDirection={{base: 'column-reverse', md: 'row'}}
+                >
+                    <Image
+                        pt={{base: '16px', md: 0}}
+                        src={useColorModeValue('/images/home/info-light.svg', '/images/home/info-dark.svg')}
+                        alignSelf={{base: 'center'}}
+                    />
+                    <List spacing={'16px'}>
+                        <ListItem>
+                            <Link href="https://twitter.com/parkeresmay" target="_blank">
+                                <Button
+                                    variant="ghost"
+                                    colorScheme="teal"
+                                    leftIcon={<FaTwitter />}
+                                >
+                                    @parkeresmay
+                                </Button>
+                            </Link>
+                        </ListItem>
+                        <ListItem>
+                            <Link href="https://github.com/thesyntaxninja" target="_blank">
+                                <Button
+                                    variant="ghost"
+                                    colorScheme="teal"
+                                    leftIcon={<FaGithub />}
+                                >
+                                    @thesyntaxninja
+                                </Button>
+                            </Link>
+                        </ListItem>
+                    </List>
+                </Flex>
+            </Animate>
         </Layout>
     )
 }
--- pages/works.js
+++ pages/works.js
@@ -6,6 +6,7 @@ import {
     useColorModeValue
 } from '@chakra-ui/react'
 import Card from '/components/Card'
+import Animate from "../components/Animate"
 
 const Works = () => {
     return (
@@ -21,24 +22,30 @@ const Works = () => {
                 Works
             </Heading>
             <SimpleGrid columns={{base: 1, md: 2}} spacing={10} mb={'3rem'}>
-                <Card
-                    link={'https://www.pocuspro.com/'}
-                    imgSrc={'/images/works/pocuspro.png'}
-                    headingText={'Pocus Pro'}
-                    descriptionText={'A SaaS product designed and developed for anonymized ultrasound review by industry experts.'}
-                />
-                <Card
-                    link={'https://www.budoagency.com/'}
-                    imgSrc={'/images/works/budo.png'}
-                    headingText={'Budo Agency'}
-                    descriptionText={'A facebook ad agency for martial arts studios.'}
-                />
-                <Card
-                    link={'https://www.apollo-se.com/'}
-                    imgSrc={'/images/works/apollose.png'}
-                    headingText={'Apollo Solar & Electric'}
-                    descriptionText={'Solar and electric installation company.'}
-                />
+                <Animate>
+                    <Card
+                        link={'https://www.pocuspro.com/'}
+                        imgSrc={'/images/works/pocuspro.png'}
+                        headingText={'Pocus Pro'}
+                        descriptionText={'A SaaS product designed and developed for anonymized ultrasound review by industry experts.'}
+                    />
+                </Animate>
+                <Animate delay={.1}>
+                    <Card
+                        link={'https://www.budoagency.com/'}
+                        imgSrc={'/images/works/budo.png'}
+                        headingText={'Budo Agency'}
+                        descriptionText={'A facebook ad agency for martial arts studios.'}
+                    />
+                </Animate>
+                <Animate delay={.2}>
+                    <Card
+                        link={'https://www.apollo-se.com/'}
+                        imgSrc={'/images/works/apollose.png'}
+                        headingText={'Apollo Solar & Electric'}
+                        descriptionText={'Solar and electric installation company.'}
+                    />
+                </Animate>
             </SimpleGrid>
             <Heading
                 fontWeight={'regular'}
@@ -51,25 +58,31 @@ const Works = () => {
                 Web3
             </Heading>
             <SimpleGrid columns={{base: 1, md: 2}} spacing={10} mb={'3rem'}>
-                <Card
-                    link={'https://www.drifternovel.com/'}
-                    imgSrc={'/images/works/drifters.png'}
-                    headingText={'Drifters of Alta'}
-                    descriptionText={'A PFP NFT based on the characters from the first full length fantasy novel ' +
-                        'on the Ethereum blockchain.'}
-                />
-                <Card
-                    link={'https://astro-dao.vercel.app/'}
-                    imgSrc={'/images/works/astrodao.png'}
-                    headingText={'Astro DAO'}
-                    descriptionText={'An NFT gated membership DAO complete with an ERC-20 token drop.'}
-                />
-                <Card
-                    link={'https://booger-boys.vercel.app/'}
-                    imgSrc={'/images/works/boogerboys.png'}
-                    headingText={'Booger Boys'}
-                    descriptionText={'A PFP NFT drop on Solana.'}
-                />
+                <Animate delay={.4}>
+                    <Card
+                        link={'https://www.drifternovel.com/'}
+                        imgSrc={'/images/works/drifters.png'}
+                        headingText={'Drifters of Alta'}
+                        descriptionText={'A PFP NFT based on the characters from the first full length fantasy novel ' +
+                            'on the Ethereum blockchain.'}
+                    />
+                </Animate>
+                <Animate delay={.5}>
+                    <Card
+                        link={'https://astro-dao.vercel.app/'}
+                        imgSrc={'/images/works/astrodao.png'}
+                        headingText={'Astro DAO'}
+                        descriptionText={'An NFT gated membership DAO complete with an ERC-20 token drop.'}
+                    />
+                </Animate>
+                <Animate delay={.6}>
+                    <Card
+                        link={'https://booger-boys.vercel.app/'}
+                        imgSrc={'/images/works/boogerboys.png'}
+                        headingText={'Booger Boys'}
+                        descriptionText={'A PFP NFT drop on Solana.'}
+                    />
+                </Animate>
             </SimpleGrid>
         </Layout>
     )
